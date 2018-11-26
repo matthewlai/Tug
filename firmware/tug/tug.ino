435908b071e727175194efd5e1936f935ce76692
Matthew Lai <m@matthewlai.ca>
2018-11-26 01:31:30 +0000

    WIP

--- firmware/tug/tug.ino
+++ firmware/tug/tug.ino
@@ -1,5 +1,3 @@
-#include <Wire.h>
-
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <LiquidCrystal_I2C.h>
@@ -22,8 +20,14 @@ const static int LS1Pin = 12;
 const static int HS2Pin = 3;
 const static int LS2Pin = 2;
 
-const static int ExtendDir = 0;
-const static int RetractDir = 1;
+#define LS1_PORT PORTB
+#define LS1_MASK _BV(6)
+
+#define LS2_PORT PORTE
+#define LS2_MASK _BV(4)
+
+const static int ExtendDir = 1;
+const static int RetractDir = 0;
 
 const static int ExtendPin = 38;
 const static int TestPin = 40;
@@ -33,29 +37,61 @@ const static int StopPin = 42;
  */
 const static int MaxCurrentError = 5;
 
+/* Amps per step in ADC reading. 175mV/A */
+const static float AmpsPerStep = 0.0279f;
+
 /* When using the simple move commands we want to make sure the actuator isn't loaded
  * significantly. The rated no load current is 2.6A. We use 4A.
  */
-const static int MaxNoLoadCurrent = 65;
+const static int MaxNoLoadCurrent = 4.0f / AmpsPerStep;
 
-/* Current at max load is 13A (213 steps). */
-const static int MaxCurrent = 213;
+/* Current at max load is 12.6A. */
+const static int MaxCurrent = static_cast<int>(12.6f / AmpsPerStep);
 
-/* Amps per step in ADC reading. 80mV/A in MLX91210 CAS-101 */
-const static float AmpsPerStep = 0.061f;
+int zero_val;
+
+volatile bool adc_data_ready = false;
+
+ISR(ADC_vect)
+{
+  adc_data_ready = true;
+}
+
+unsigned int read_current_raw() {
+  while (!adc_data_ready) {}
+  adc_data_ready = false;
+
+  // must read ADCL before ADCH
+  unsigned char low_byte = ADCL;
+  unsigned char high_byte = ADCH;
+  unsigned int val = high_byte;
+  val <<= 8;
+  val |= low_byte;
+  return val;
+}
 
 /* Returns current in steps, positive or negative depending on direction.
  */
 int read_current() {
-  return analogRead(CurrentSensorPin) - 512;
+  return read_current_raw() - zero_val;
+}
+
+void calibrate_zero_current() {
+  const int kNumReadings = 100;
+  const int kDelayBetweenReadingsMs = 10;
+  long sum = 0;
+  for (int i = 0; i < kNumReadings; ++i) {
+    sum += read_current_raw();
+    delay(kDelayBetweenReadingsMs);
+  }
+
+  zero_val = int(sum / long(kNumReadings));
+  Serial.print("Zero val: ");
+  Serial.println(zero_val);
 }
 
 int current_to_kg(float current) {
-  return (int) (35.09f * current - 22.456f); // from calibration
-}
-
-int current_to_newton(float current) {
-  return (int) ((35.09f * current - 22.456f) * 9.81f); // from calibration
+  return (int) (40.3f * current - 31.3f); // from calibration
 }
 
 bool read_extend_button() {
@@ -80,8 +116,8 @@ void turn_off_all_gates() {
 
 // dir = 0 is HS1, LS2.
 // dir = 1 is HS2, LS1.
-int dir = 0;
-int ls_pin = LS2Pin;
+byte dir = 0;
+byte ls_pin = LS2Pin;
 void set_dir(int new_dir) {
   turn_off_all_gates();
 
@@ -97,34 +133,39 @@ void set_dir(int new_dir) {
 }
 
 void drive_motor(bool val) {
-  digitalWrite(ls_pin, val ? HIGH : LOW);
+  if (dir == 0) {
+    if (val) {
+      LS2_PORT |= LS2_MASK;
+    } else {
+      LS2_PORT &= ~LS2_MASK;
+    }
+  } else {
+    if (val) {
+      LS1_PORT |= LS1_MASK;
+    } else {
+      LS1_PORT &= ~LS1_MASK;
+    }
+  }
 }
 
-char lcd_buffer[4][20];
+const static int kLcdHeight = 4;
+const static int kLcdWidth = 20;
+
+char lcd_buffer[kLcdHeight][kLcdWidth];
 byte lcd_x = 0;
 byte lcd_y = 0;
 
 void init_lcd_buffer() {
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 20; ++j) {
+  for (int i = 0; i < kLcdHeight; ++i) {
+    for (int j = 0; j < kLcdWidth; ++j) {
       lcd_buffer[i][j] = ' ';
     }
   }
 }
 
-void lcd_print(const char *s) {
-  lcd.print(s);
-  size_t len = strlen(s);
-  for (size_t i = 0; i < len; ++i) {
-    lcd_buffer[lcd_y][lcd_x + i] = s[i];
-  }
-  lcd_x += len;
-}
-
-void lcd_print(char c) {
-  lcd_buffer[lcd_y][lcd_x] = c;
-  lcd.write(c);
-  ++lcd_x;
+void lcd_return_to_start_of_line() {
+  lcd_x = 0;
+  lcd.setCursor(lcd_x, lcd_y);
 }
 
 void lcd_newline() {
@@ -133,16 +174,16 @@ void lcd_newline() {
     lcd.clear();
 
     // Copy every line up.
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < (kLcdHeight - 1); ++i) {
       lcd.setCursor(0, i);
-      for (int c = 0; c < 20; ++c) {
+      for (int c = 0; c < kLcdWidth; ++c) {
         lcd.write(lcd_buffer[i+1][c]);
         lcd_buffer[i][c] = lcd_buffer[i+1][c];
       }
     }
 
     // Clear the last line.
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < kLcdWidth; ++i) {
       lcd_buffer[3][i] = ' ';
     }
 
@@ -156,9 +197,27 @@ void lcd_newline() {
   }
 }
 
-void lcd_return_to_start_of_line() {
-  lcd_x = 0;
-  lcd.setCursor(lcd_x, lcd_y);
+void lcd_newline_if_at_end() {
+  if (lcd_x >= kLcdWidth) {
+    lcd_newline();
+  }
+}
+
+void lcd_print(const char *s) {
+  lcd.print(s);
+  size_t len = strlen(s);
+  for (size_t i = 0; i < len; ++i) {
+    lcd_newline_if_at_end();
+    lcd_buffer[lcd_y][lcd_x] = s[i];
+    ++lcd_x;
+  }
+}
+
+void lcd_print(char c) {
+  lcd_newline_if_at_end();
+  lcd_buffer[lcd_y][lcd_x] = c;
+  lcd.write(c);
+  ++lcd_x;
 }
 
 void move_unloaded(int dir) {
@@ -168,21 +227,14 @@ void move_unloaded(int dir) {
   
   set_dir(dir);
   drive_motor(true);
-  while (true) {
-    for (int i = 0; i < 1000; ++i) {
-      // Total = 50us
-      drive_motor(true);
-      delayMicroseconds(25);
-      drive_motor(false);
-      delayMicroseconds(25);
-    }
+  while (!read_stop_button()) {
     int current = read_current();
     int abs_current = abs(current); /* abs is implemented as a macro on arduino... silly! */
     filtered_current = filtered_current * 0.8f + float(abs_current) * 0.2f;
     Serial.println(filtered_current * AmpsPerStep);
     
     if (filtered_current < MaxCurrentError) {
-      //++stop_count;
+      ++stop_count;
       if (stop_count >= 5) {
         break; /* we have reached the end (current fell to 0) */
       }
@@ -201,26 +253,16 @@ void move_unloaded(int dir) {
   drive_motor(false);
 }
 
-int drive(int current_setting, int dir, unsigned long end_time_ms) {
+int drive(int current_setting, unsigned long end_time_ms) {
   unsigned int averaging_window[64] = { 0 };
   byte next_index = 0;
   unsigned int sum = 0;
-
-  current_setting = 20;
-
-  if (dir == ExtendDir) {
-    current_setting *= -1;
-  }
   
   while (millis() < end_time_ms) {
     int current = read_current();
-    if ((current > current_setting && dir == ExtendDir) || (current < current_setting && dir == RetractDir)) {
-      drive_motor(true);
-    } else {
-      drive_motor(false);
-    }
+    int abs_current = abs(current);
 
-    unsigned int abs_current = abs(current);
+    drive_motor(abs_current < current_setting);
 
     sum -= averaging_window[next_index];
     sum += abs_current;
@@ -234,7 +276,7 @@ int drive(int current_setting, int dir, unsigned long end_time_ms) {
 
 void calibrate(int dir, int current_setting) {
   set_dir(dir);
-  int final_current = drive(current_setting, dir, millis() + 15000);
+  int final_current = drive(current_setting, millis() + 15000);
   Serial.print(final_current * AmpsPerStep * 1000);
   Serial.println(" mA");
 }
@@ -242,10 +284,15 @@ void calibrate(int dir, int current_setting) {
 // Fast int to str that only works from 0 to 999. s must be big enough to hold 4 digits.
 void lcd_print_int(int x) {
   if (x < 0) {
+    lcd_print(' ');
+    lcd_print(' ');
     lcd_print('0');
   } else if (x < 10) {
+    lcd_print(' ');
+    lcd_print(' ');
     lcd_print(x + '0');
   } else if (x < 100) {
+    lcd_print(' ');
     lcd_print(x / 10 + '0');
     lcd_print(x % 10 + '0');
   } else if (x < 1000) {
@@ -254,7 +301,7 @@ void lcd_print_int(int x) {
     lcd_print((x % 10) + '0');
   } else {
     lcd_print("ERR");
-    lcd_print_int(x - 1000);
+    lcd_print_int(x / 100);
   }
 }
 
@@ -271,23 +318,21 @@ bool test(int dir) {
   bool stopped = false;
 
   lcd_newline();
+  lcd_print("    kg");
 
   while (iteration < 256 && !(stopped = read_stop_button())) {
-    int avg_current = drive(current_setting, dir, millis() + 200);
+    int avg_current = drive(current_setting, millis() + 100);
 
     if (avg_current > max_current) {
       max_current = avg_current;
     }
 
-// Too slow and will interrupt motor driving.
-#if 0
     char buf[64];
     sprintf(buf, "%d %d", current_setting, avg_current);
     Serial.println(buf);
 
-    //lcd_return_to_start_of_line();
-    //lcd_print_int(static_cast<int>(current_to_kg(avg_current * AmpsPerStep)));
-#endif   
+    lcd_return_to_start_of_line();
+    lcd_print_int(static_cast<int>(current_to_kg(avg_current * AmpsPerStep)));
 
     current_settings[iteration] = current_setting;
     avg_currents[iteration] = avg_current;
@@ -308,10 +353,11 @@ bool test(int dir) {
     if (current_setting > avg_current) {
       // wait for current to catch up
     } else {
-      current_setting += 1; // 0.05A per 200 millisecond
+      current_setting += 4;
 
       if (current_setting > MaxCurrent) {
         Serial.println("Max current reached");
+        lcd_return_to_start_of_line();
         lcd_print("Max current reached.");
         break;
       }
@@ -333,6 +379,7 @@ bool test(int dir) {
 
   sprintf(buf, "%d", (int) current_to_kg(max_current * AmpsPerStep));
 
+  lcd_newline();
   lcd_print(buf);
   lcd_print(" kg. ");
 
@@ -346,7 +393,6 @@ void setup() {
   Serial.setTimeout(10);
   lcd.begin(20,4);
   init_lcd_buffer();
-  lcd_print("Ready");
 
   pinMode(CurrentSensorPin, INPUT_PULLUP);
 
@@ -367,12 +413,26 @@ void setup() {
   pinMode(TestPin, INPUT_PULLUP);
   pinMode(StopPin, INPUT_PULLUP);
 
-  // set prescaler to 16 (77kHz sampling rate, 13 microseconds)
-  sbi(ADCSRA,ADPS2);
-  cbi(ADCSRA,ADPS1);
-  cbi(ADCSRA,ADPS0);
+  adc_data_ready = false;
 
-  Serial.println("Reset");
+  // Set up the ADC
+  ADCSRA = 0;
+  ADCSRB = 0; // free-running mode
+  ADMUX |= (0 & 0x07); // set A0 analog input pin
+  ADMUX |= (1 << REFS0); // set reference voltage
+  ADCSRA |= (1 << ADEN);  // enable ADC
+
+  ADCSRA |= (1 << ADPS2); // 16 prescaler for 76.9 KHz
+
+  ADCSRA |= (1 << ADATE); // enable auto trigger
+  ADCSRA |= (1 << ADIE);  // enable interrupts when measurement complete
+  ADCSRA |= (1 << ADSC);  // start ADC measurements
+
+  sei();
+  calibrate_zero_current();
+
+  Serial.println("Reset done.");
+  lcd_print("Ready");
 }
 
 void loop() {  
@@ -419,7 +479,9 @@ void loop() {
       }
       Serial.print("Calibrating at ");
       Serial.println(current_setting);
-      move_unloaded(RetractDir);
+      Serial.println("Extending");
+      move_unloaded(ExtendDir);
+      Serial.println("Calibrating");
       calibrate(RetractDir, current_setting);
     }
     Serial.println("Done");
